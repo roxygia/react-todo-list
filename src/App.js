da94169fe02bc7be16c1aa0fc8e857aa804d5a2a
roxygia <62426542+roxygia@users.noreply.github.com>
2020-09-12 11:47:24 +0800

    add todo list and form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import TodoItem from "./components/TodoItem"
-
+import TodoForm from "./components/TodoForm"
 
 function App(){
+//variables
+const [todos, setTodos] = useState([
+  "Finish plus project",
+  "Feed Peggy",
+  "Sleep"
+])
+
+//method
+const addTodo = (text) => {
+  const newTodos = [...todos,text];
+  setTodos(newTodos);
+};
+
   return (
     <div>
       <h1>My todo list</h1>;
-      <TodoItem todo="Finish plus project."/>
-      <TodoItem todo="Feed peggy."/>
-      <TodoItem todo="Sleep."/>
+      {todos.map((todo, index) => (
+        <TodoItem todo={todo} key={index} />
+        
+      ))}
+      <TodoForm addTodo={addTodo}/>
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+
+function TodoForm(props) {
+    const {addTodo} = props;
+    const [value, setValue] = useState("");
+
+    //methods
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!value) {
+            return;
+        }
+
+    addTodo(value);
+    setValue("");
+
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+            value={value}
+            type="text" 
+            placeholder="Add todo..."
+            onChange={(event) => setValue(event.target.value)}
+            
+        />
+        </form>
+    )
+}
+
+export default TodoForm;
