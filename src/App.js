2c708422338b912a4b9d28d01e356d6824a9ebc3
roxygia <62426542+roxygia@users.noreply.github.com>
2020-09-12 11:15:58 +0800

    created react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoItem from "./components/TodoItem"
 
-function App() {
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My todo list</h1>;
+      <TodoItem todo="Finish plus project."/>
+      <TodoItem todo="Feed peggy."/>
+      <TodoItem todo="Sleep."/>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
