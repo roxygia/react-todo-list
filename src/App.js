6b682ce905579d6965c5cf02f95b57fd9e9ed0e7
roxygia <62426542+roxygia@users.noreply.github.com>
2020-09-12 12:33:40 +0800

    added complete and remove buttons

--- src/App.js
+++ src/App.js
@@ -6,23 +6,41 @@ import "./App.css"
 function App(){
 //variables
 const [todos, setTodos] = useState([
-  "Finish plus project",
-  "Feed Peggy",
-  "Sleep"
-])
+  {text:"Finish plus project", isCompleted: false },
+  {text:"Feed Peggy", isCompleted: false },
+  {text:"Sleep", isCompleted: false },
+]);
 
 //method
 const addTodo = (text) => {
-  const newTodos = [...todos,text];
+  const newTodos = [...todos,{text}];
   setTodos(newTodos);
 };
+
+const completeTodo = (index) => {
+  const newTodos = [...todos];
+  newTodos[index].isCompleted = true;
+  setTodos(newTodos);
+}
+
+const removeTodo = (index) => {
+  const newTodos = [...todos];
+  newTodos.splice(index,1);
+  setTodos(newTodos);
+}
 
   return (
     <div className="app">
       <div className="todo-list">
         <h1>My todo list</h1>
         {todos.map((todo, index) => (
-          <TodoItem todo={todo} key={index} />
+          <TodoItem 
+          todo={todo} 
+          key={index} 
+          completeTodo={completeTodo} 
+          index={index} 
+          removeTodo={removeTodo} 
+          />
           
         ))}
         <TodoForm addTodo={addTodo}/>
--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import "./TodoItem.css"
 
 function TodoItem(props) {
-    const {todo} = props;
-    return <div className="todo">{todo}</div>
+    const {todo, index, completeTodo, removeTodo} = props;
+
+    return (
+    <div className={`todo ${todo.isCompleted ? "complete": ""}`}>
+        {todo.text}
+        <div>
+            <button onClick={() => completeTodo(index)}>Complete</button>
+            <button onClick={() => removeTodo(index)}>Remove</button>
+        </div>
+    </div>
+    )
 }
 
 export default TodoItem
